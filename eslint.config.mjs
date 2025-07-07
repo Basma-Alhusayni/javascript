aef6903b36f1b5804df46a968ff5b877ad805beb
BasmaAlhusayni <tu4358236@taibahu.edu.sa>
2025-07-07 12:50:53 +0300

    Adding the Eslint rules from the repo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,36 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      quotes: ['error', 'single'],
+      'no-var': 'error',
+      eqeqeq: 'error',
+      'no-unused-vars': 'error',
+      'default-case': 'error',
+      camelcase: 'error',
+      'consistent-return': 'error',
+      'max-depth': ['error', 2],
+      'prefer-arrow-callback': 'error',
+      'prefer-const': 'error',
+      'no-useless-return': 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'script',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
 ]);
